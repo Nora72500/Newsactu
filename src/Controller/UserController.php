5af220ae83c1346f1e09f38bc012ed7296771eee
Nora72500 <104192019+Nora72500@users.noreply.github.com>
2022-07-22 14:52:12 +0200

    Update UserController.php

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,108 +5,91 @@ namespace App\Controller;
 use DateTime;
 use App\Entity\User;
 use App\Entity\Article;
+use App\Form\ChangePasswordFormType;
 use App\Form\RegisterFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserController extends AbstractController
 {
     /**
-     * @Route("/inscription", name="user_register", methods={"GET|POST"})
+     * @Route ("/inscription", name="user_register", methods={"GET|POST"})
      */
-    public function register(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    public function register (Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
-        # 1 - Instanciation de class
+        #1-instanciation de class
         $user = new User();
 
-        # 2 - Création du formulaire
-        $form = $this->createForm(RegisterFormType::class, $user)->handleRequest($request);
+        #2-Création de formulaire
+        $form = $this->createForm(RegisterFormType::class, $user)
+            ->handleRequest($request);
 
-        # 4 - Si le form est soumis ET valide
+        #4-SI le form est soumis et valide
         if($form->isSubmitted() && $form->isValid()) {
             $user->setRoles(['ROLE_USER']);
             $user->setCreatedAt(new DateTime());
-            $user->setUpdatedAt(new Datetime());
+            $user->setUpdatedAt(new DateTime());
 
-            $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
+            $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword())); //version plus courte que le plainPassword
 
             $entityManager->persist($user);
             $entityManager->flush();
 
-            $this->addFlash('success', "Vous vous étes inscrit avec succès !");
-            return $this->redirectToRoute('app_login');
+            $this->addFlash('success', "Vous vous êtes inscrit avec succès !");
+            return $this->redirectToRoute('app_login'); 
+            
         }
-
-        # 3 - On retourne la vue du formulaire
+        #3- On retourne la vue du formulaire
         return $this->render("user/register.html.twig", [
-            'form' => $form->createView()    
+            'form' => $form->createView()
+        ]);
+
+    }
+
+    /**
+    * @Route("/profile/mon-espace-perso", name="show_profile", methods={"GET"})
+    */
+    public function showProfile(EntityManagerInterface $entityManager): Response
+    {
+        $articles = $entityManager->getRepository(Article::class)->findBy(['author'=> $this->getUser()]);
+        
+        return $this->render("user/show_profile.html.twig", [
+            'articles'=> $articles
         ]);
     }
 
     /**
-     * @Route("/profile/mon-espace-perso", name="show_profile", methods={"GET"})
+     * @Route("/profile/changer-mon-mot-de-passe", name="change_password", methods={"GET|POST"})
      */
-    public function showProfile(EntityManagerInterface $entityManager): Response
+    public function changePassword(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
-        $articles = $entityManager->getRepository(Article::class)->findBy(['author' => $this->getUser()]);
+        $form = $this->createForm(ChangePasswordFormType::class)
+            ->handleRequest($request);
 
-        return $this->render("user/show_profile.html.twig", [
-            'articles' => $articles
+        if($form->isSubmitted() && $form->isValid()) {
+            
+            $user = $entityManager->getRepository(User::class)->findOneBy(['id' => $this->getUser()]);
+
+            $user->setUpdatedAt(new DateTime());
+
+            $user->setPassword($passwordHasher->hashPassword(
+                $user, $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', "Votre mot de passe a bien été changé");
+            return $this->redirectToRoute('show_profile');
+        }
+
+        return $this->render('user/change_password.html.twig', [
+            'form' => $form->createView()
         ]);
     }
-
-
-/**
-    * @Route("/profile/changer-mon-mot-de-passe", name="change_password", methods= {"GET|POST"})
-    */
-/**
-    * @Route("/profile/changer-mon-mot-de-passe/{id}", name="change_password", methods= {"GET|POST"})
-    */
-    public function changePassword (EntityManagerInterface $entityManager, UserPasswordHasher $passwordHasher, Request $request):Response
-    {
-         $form = $this->createForm()
-         ->handleRequest($request);
- 
-         if($form->isSubmitted() && $form->isValid()) {
-            /** @var User $user */
-            $user = $entityManager->getRepository(user::class)->findOneBy(['id' => $this->getUser() ]);
-
-
-             $user->setPassword($passwordHasher->hashPassword
-             ($user, $form->get('plainPassword'=->getData->getPassword());
-         ));
-            $this->addFlash('success', "votre mot de passe a bien etait changer");
-            return $this->redirectToRoute('show_profile');
-         }
-         
-
-         $entityManager->persist($user);
-         $entityManager->flush();
-         return $this->render('Change_Password.html.twig', [
-             'form'=> $form->createView(),
-              => 'modif'
-         ]);
-    }
- }
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
